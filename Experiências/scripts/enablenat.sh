335c29607e56e82ea4c0731b80a67af212b09202
Sk0ut <poke_flavio@hotmail.com>
2015-12-22 23:36:05 +0000

    Fix and comment scripts

--- Experiências/scripts/enablenat.sh
+++ Experiências/scripts/enablenat.sh
@@ -1,3 +1,2 @@
 iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
-iptables -A FORWARD -i eth1 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
+iptables -A FORWARD -i eth1 -m state --state NEW,INVALID -j DROP
