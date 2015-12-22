e684f9edb89d1a2e841fc5ece47aef9fc60afbe6
Sk0ut <poke_flavio@hotmail.com>
2015-12-22 23:48:54 +0000

    Comment nat commands

--- Experiências/scripts/enablenat.sh
+++ Experiências/scripts/enablenat.sh
@@ -1,2 +1,4 @@
-iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+#Translate addresses going out through interface eth1
+iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE 
+#Drop NEW and INVALID packets entering through interface eth1
 iptables -A FORWARD -i eth1 -m state --state NEW,INVALID -j DROP
